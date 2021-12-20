e892e46401b2f06a5ed96104925bf1dd09402d17
Adam Brewer <adamhb321@gmail.com>
2021-12-20 13:33:54 -0500

    Updates

--- src/static/js/game.js
+++ src/static/js/game.js
@@ -1,9 +1,22 @@
-const ASSETSDIR = "/assets/";
 function main(){
-    let app = new PIXI.Application({ width: 640, height: 360 });
+    const app = new PIXI.Application({
+        autoResize: true,
+        resolution: devicePixelRatio 
+    });
     document.body.appendChild(app.view);
-    let sprite = PIXI.Sprite.from(ASSETSDIR + "entities/enemies/GenericEnemy01.png");
+    let sprite = PIXI.Sprite.from("/assets/entities/enemies/GenericEnemy01.png");
     app.stage.addChild(sprite);
+    // Listen for window resize events
+    let resize = () => {
+        // Resize the renderer
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+        // You can use the 'screen' property as the renderer visible
+        // area, this is more useful than view.width/height because
+        // it handles resolution
+        rect.position.set(app.screen.width, app.screen.height);
+    }
+    window.addEventListener('resize', resize);
+    resize()
 }
 
 main();
