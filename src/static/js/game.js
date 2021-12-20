ff7ddf4cf39ac6a824ed833ed3b221c0e2e6cdc8
Adam Brewer <adamhb321@gmail.com>
2021-12-20 13:39:37 -0500

    Renderer sizing fix

--- src/static/js/game.js
+++ src/static/js/game.js
@@ -1,22 +1,8 @@
 function main(){
-    const app = new PIXI.Application({
-        autoResize: true,
-        resolution: devicePixelRatio 
-    });
+    const app = new PIXI.Application({resizeTo: window, resizeThrottle: 250 });
     document.body.appendChild(app.view);
     let sprite = PIXI.Sprite.from("/assets/entities/enemies/GenericEnemy01.png");
     app.stage.addChild(sprite);
-    // Listen for window resize events
-    let resize = () => {
-        // Resize the renderer
-        app.renderer.resize(window.innerWidth, window.innerHeight);
-        // You can use the 'screen' property as the renderer visible
-        // area, this is more useful than view.width/height because
-        // it handles resolution
-        rect.position.set(app.screen.width, app.screen.height);
-    }
-    window.addEventListener('resize', resize);
-    resize()
 }
 
 main();
