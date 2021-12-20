c68b086cc06f9eac0e6dcc08689a07ea74d0e326
Adam Brewer <adamhb321@gmail.com>
2021-12-20 12:59:55 -0500

    init

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 6969
+// Setup static content serving middleware
+app.use(express.static('static'))
+const HTMLDIR = __dirname + '/static' + '/html'
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(HTMLDIR + '/index.html')
+})
+// Launch server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
